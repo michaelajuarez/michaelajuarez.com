203806185bdcc35743e7e802868144f8130dce48
michaelajuarez <michaelajuarez@gmail.com>
2022-09-13 21:05:55 -0700

    Added beginnings of Nav header

---
+++ src/pages/nav.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+
+// Grey: #8a8787
+// Dark Blue: #6A7C8C
+
+const navBar = {
+  backgroundColor: "#6A7C8C",
+  width: "100%",
+  height: "400px",
+  display: "inline-block",
+}
+
+const text = {
+  marginTop: "0px",
+  marginBottom: "0px",
+  paddingLeft: "2px",
+}
+
+const Nav = () => {
+  return (
+    <div style={navBar}>
+    </div>
+  )
+}
+
+export default Nav
